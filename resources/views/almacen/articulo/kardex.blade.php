70fdd18fa8f6093b623fb24adc323186b709a55e
luis1612 <ibarguen1294@gmail.com>
2019-10-06 11:56:09 -0500

    actualizacion

--- resources/views/almacen/articulo/kardex.blade.php
+++ resources/views/almacen/articulo/kardex.blade.php
@@ -2,7 +2,7 @@
 @section ('contenido')
 <div class="row">
   <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-    <h3>Kardex del producto : <strong>" {{ $articulo->nombre }} - {{ $articulo->codigo }} - {{ $articulo->contenido }}- {{ $articulo->bodega }}" - </strong>  Existencia Actual :  <strong> {{ $articulo->stock }}</strong> </h3> 
+    <h3>Kardex del producto : <strong>" {{ $articulo->nombre }} - {{ $articulo->codigo }} - {{ $articulo->contenido }}-<H3> Bodega  </H3> {{ $articulo->bodega }}" - </strong>  Existencia Actual :  <strong> {{ $articulo->stock }}</strong> </h3> 
   </div>
 </div>
 
